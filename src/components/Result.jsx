c57d0c72213f6455e2c5e6fa1ec1d76eef8879d4
pam0701 <psb04027@naver.com>
2022-10-21 17:16:00 +0900

    git test2

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,1 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
+
+class Result extends Component {
+  render() {
+    return (
+      <div>
+        <div className="result-title">
+          <div>혜영님의 2022</div>
+        </div>
+        <div className="result-list">
+          <h3>올해 당신이 뽑은 최고의 드라마는?</h3>
+          <p>이상한 변호사 우영우</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Result;
