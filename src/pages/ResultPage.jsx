f94b4fc9b6a00d85b257a3e6a077be218e5b947d
lkf6214 <107837292+lkf6214@users.noreply.github.com>
2022-10-31 14:04:17 +0900

    지현

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,5 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Diarymode from '../components/Diarymode';
+import { useEffect, useState } from 'react';
+
 import Header from '../components/common/Header';
 import Result from '../components/Result';
 import CommentForm from '../components/CommentForm';
